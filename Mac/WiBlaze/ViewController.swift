5521b1e607a0cee5e298592965bfa4bdad164753
Justin Bush <jaybush7@gmail.com>
2015-06-28 12:07:42 -0400

    Initial commit

---
+++ Mac/WiBlaze/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiBlaze
+//
+//  Created by Justin Bush on 2015-06-28.
+//  Copyright © 2015 Justin Bush. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

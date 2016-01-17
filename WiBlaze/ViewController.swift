09c530b466f7fe761495481abec0d4cee27ab03e
Justin Bush <jaybush7@gmail.com>
2016-01-17 15:52:45 -0500

    Attempt at adding custom loader
    Custom loader uses a WKWebView, along with a local SVG.

--- WiBlaze/ViewController.swift
+++ WiBlaze/ViewController.swift
@@ -12,12 +12,23 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var loadingView: WKWebView!
     @IBOutlet var textField: UITextField!
 
     override func loadView() {
+        view = webView
         webView = WKWebView()
         webView.navigationDelegate = self
-        view = webView
+        webView.backgroundColor = UIColor.whiteColor()
+        webView.opaque = true
+        
+        view = loadingView
+        loadingView = WKWebView()
+        loadingView.navigationDelegate = self
+        loadingView.hidden = true
+        loadingView.alpha = 0.5
+        loadingView.backgroundColor = UIColor.whiteColor()
+        
         UIApplication.sharedApplication().statusBarStyle = .Default
     }
     
@@ -27,6 +38,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = NSURL(string: "about:blank")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        let loadingURL = NSBundle.mainBundle().URLForResource("LoaderPhone", withExtension:"html")!
+        self.loadingView.loadRequest(NSURLRequest(URL: loadingURL))
         
         // Notification observer for textField
         self.textField.addTarget(self, action: "textFieldDidUpdate:", forControlEvents: UIControlEvents.EditingChanged)
@@ -58,28 +72,60 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
+    /*
     func textFieldDidUpdate(sender: UITextField) {
-        if (textField.text!.rangeOfCharacterFromSet(NSCharacterSet.whitespaceCharacterSet()) != nil) {
         guard
             let text = sender.text,
             query = text.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet()),
             url = NSURL(string: "https://google.com/#q=\(query)")
-            else { return }
+            else {
+                if (sender.text?.rangeOfCharacterFromSet(NSCharacterSet.whitespaceCharacterSet()) != nil) {
+                    NSURL.validateUrl(sender.text, completion: { (success, urlString, error) -> Void in
+                        dispatch_async(dispatch_get_main_queue(), { () -> () in
+                            if (success) {
+                                self.webView.hidden = false
+                                guard
+                                    let urlString = urlString,
+                                    let requestURL = NSURL(string: urlString)
+                                    else { return }
+                                self.webView.loadRequest(NSURLRequest(URL: requestURL))
+                            } else {
+                                self.webView.stopLoading()
+                            }
+                        })
+                    })
+                }
+                return
+        }
+        
         webView.hidden = false
         webView.loadRequest(NSURLRequest(URL: url))
+    }
+    */
+    
+    func textFieldDidUpdate(textField: UITextField) {
+        if (textField.text!.rangeOfCharacterFromSet(NSCharacterSet.whitespaceCharacterSet()) != nil) {
+            guard
+                let text = textField.text,
+                query = text.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet()),
+                url = NSURL(string: "https://google.com/#q=\(query)")
+                else { return }
+            loadingView.hidden = true
+            webView.loadRequest(NSURLRequest(URL: url))
         
         } else {
             // Validate URL
             NSURL.validateUrl(textField.text, completion: { (success, urlString, error) -> Void in
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     if (success) {
-                        self.webView.hidden = false
+                        self.loadingView.hidden = true
                         let request = NSURLRequest(URL: NSURL(string: urlString!)!)
                         print(urlString!)
                         self.webView.loadRequest(request)
                     } else {
                         self.webView.stopLoading()
-                        //self.webView.hidden = true
+                        self.loadingView.hidden = false
+                        print("View is hidden")
                     }
                 })
             })
